6da87fa4446b469171dafc3f51586a6051c1d97b
KhayT <tkprof4@gmail.com>
2022-10-26 16:34:21 -0700

    Completed Task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer";
+import Links from "./components/Links";
+import img from "./images/khadijah.jpg";
+import slackIcon from "./images/slack.jpg";
+import githubIcon from "./images/github.jpg";
+import shareIcon from "./images/share icon.svg";
+import dots from "./images/dots-horizontal.svg";
+import cameraIcon from "./images/camera icon.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <div className="shareBtn-container">
+        <div className="tooltip">
+          <div className="content">
+            <p>Share Link</p>
+          </div>
+          <div className="bottom-center"></div>
+        </div>
+
+        <img src={shareIcon} alt="" id="share-btn" />
+
+        <img src={dots} alt="" id="dots" />
+      </div>
+
+      <div className="image-container">
+        <img src={img} alt="profileImage" id="profile__img" />
+
+        <div className="onhover">
+          <img src={cameraIcon} alt="" id="cameraIcon" />
+        </div>
+      </div>
+
+      <p id="twitter">Abdulazeez_KT</p>
+      <p id="slack">Khadijah</p>
+
+      <div className="link-container">
+        <Links
+          url="https://twitter.com/Abdulazeez_KT"
+          title="Twitter Link"
+          id="twiter-link"
+        />
+
+        <Links
+          url="https://training.zuri.team/"
+          title="Zuri Team"
+          id="btn__zuri"
+        />
+
+        <Links url="http://books.zuri.team" title="Zuri Books" id="books" />
+
+        <Links
+          url="http://books.zuri.team"
+          title="Python Books"
+          id="book__python"
+        />
+
+        <Links
+          url="https://background.zuri.team"
+          title="Background Check for Coders"
+          id="pitch"
+        />
+
+        <Links
+          url="https://books.zuri.team/design-rules"
+          title="Design Books"
+          id="book__design"
+        />
+      </div>
+
+      <div className="icons-container">
+        <a href="https://slack.com" target="blank">
+          <img src={slackIcon} alt="" />
         </a>
-      </header>
-    </div>
+
+        <a href="https://github.com/khayt" target="blank">
+          <img src={githubIcon} alt="" />
+        </a>
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
